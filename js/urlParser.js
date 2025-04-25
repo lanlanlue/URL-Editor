848182d0e018b115b5dd470cbb66446d6a581c3e
Darren Lan <darren.lan@digitalforset.com.tw>
2025-04-25 15:51:21 +0800

    :art: first commit

---
+++ js/urlParser.js
@@ -0,0 +1,16 @@
+export function validateUrl(input) {
+  try {
+    const url = new URL(input);
+    return { valid: true, url };
+  } catch (e) {
+    return { valid: false, error: "無效的 URL 格式" };
+  }
+}
+
+export function parseUrl(url) {
+  return {
+    domain: url.hostname,
+    path: url.pathname,
+    params: Array.from(url.searchParams.entries()), // [['foo', 'bar'], ['id', '123']]
+  };
+}
