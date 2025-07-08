05d11f74a85b9d9127a57380c4d8d0f735706238
lanlanlue <lanlanlue@users.noreply.github.com>
2025-07-08 09:22:04 +0000

    deploy: 3cd0137b3edb7483237f5f713b009f5a50c98abd

---
+++ URL-Editor.5cf9e66e.js
@@ -0,0 +1,2 @@
+(window.location.protocol.startsWith("chrome-extension:")||window.location.protocol.startsWith("moz-extension:"))&&document.documentElement.classList.add("is-extension-popup");
+//# sourceMappingURL=URL-Editor.5cf9e66e.js.map
